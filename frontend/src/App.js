5420dee9a5fd7204d33643ae7045095e0bc5fdb6
willteowh <10542244+willteowh@users.noreply.github.com>
2024-06-13 01:07:18 +0800

    move Route into 1 file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,11 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
-import About from "./pages/About";
+import RoutesConfig from "./Routes";
 
 const App = () => {
   return (
-    <Router>
-      <div className="container mx-auto">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="container mx-auto">
+      <RoutesConfig />
+    </div>
   );
 };
 
---
+++ frontend/src/Routes.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Navbar from "./components/Navbar";
+
+const RoutesConfig = () => {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default RoutesConfig;
